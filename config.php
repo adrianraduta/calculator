54c7a3065a612ec33f7f5190347fe2baa35d1b57
Andrei Baloleanu <baloleanu.andrei@gmail.com>
2020-10-27 13:25:25 +0200

    import moved to config.php

--- config.php
+++ config.php
@@ -14,4 +14,15 @@ if ($conn->connect_error) {
 }
 $message = "true";
 
+$sql = "SELECT ID, Nume, Email FROM Senders";
+$result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["ID"]. " - Name: " . $row["Nume"]. " " . $row["Email"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
