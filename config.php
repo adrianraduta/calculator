1d2dd6240740d78dbfbc2790703007682f8b7151
Andrei Baloleanu <baloleanu.andrei@gmail.com>
2020-10-27 12:01:37 +0200

    sss

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$username = "ispdec3h1kmywx58";
+$password = "wffu1yoh6mc2dqy2";
+$message = "blank";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+$message = "false";
+}
+$message= "true";
--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
-$servername = "j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-$username = "ispdec3h1kmywx58";
-$password = "wffu1yoh6mc2dqy2";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+echo $message;
