c8c6b468db5a4fe7954d528e05b227503519e031
Andrei Baloleanu <baloleanu.andrei@gmail.com>
2020-10-27 12:16:34 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,15 +2,12 @@
 $servername = "j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
 $username = "ispdec3h1kmywx58";
 $password = "wffu1yoh6mc2dqy2";
+$message = "blank";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
-global $message;
-$message = "blank";
     die("Connection failed: " . $conn->connect_error);
-$message = "fail";
 }
-$message = "true";
