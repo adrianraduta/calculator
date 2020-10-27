c64ff41daf190e5013507b0d92651c776edc4816
Andrei Baloleanu <baloleanu.andrei@gmail.com>
2020-10-27 11:40:57 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+$servername = "j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$username = "ispdec3h1kmywx58";
+$password = "wffu1yoh6mc2dqy2";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
