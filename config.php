fdb299d3a5a746f53f748b088faceb4f7de6248d
Andrei Baloleanu <baloleanu.andrei@gmail.com>
2020-10-27 13:32:16 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,15 +5,15 @@ $password = "wffu1yoh6mc2dqy2";
 $database = "t2szimexkozly7rg";
 $message = "blank";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 	$message = "fail";
 }
 $message = "true";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
 
 $sql = "SELECT ID, Nume, Email FROM Senders";
 $result = $conn->query($sql);
