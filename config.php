a5f11d4c60201c3c72e63459cb2385490691a4e0
Andrei Baloleanu <baloleanu.andrei@gmail.com>
2020-10-27 12:06:10 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 $servername = "j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
 $username = "ispdec3h1kmywx58";
 $password = "wffu1yoh6mc2dqy2";
+global $message;
 $message = "blank";
 
 // Create connection
